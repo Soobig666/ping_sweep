cd733b8eadd84e7634ad43fa6eb1a79b1f92c456
soobig666 <darkvader.ukr.net@gmail.com>
2023-01-04 12:16:31 +0200

    before RESTRUCTURE

--- main.py
+++ main.py
@@ -1,38 +1,45 @@
 import os
 import platform
 
+from time import sleep
 from datetime import datetime
 
-net = input("Enter the Network Address: ")
-net1 = net.split('.')
-a = '.'
 
-net2 = net1[0] + a + net1[1] + a + net1[2] + a
-st1 = int(input("Enter the Starting Number: "))
-en1 = int(input("Enter the Last Number: "))
-en1 = en1 + 1
-op_system = platform.system()
+while True:
+    host = input("Введите адрес сети: ")
+    splited_host = host.split('.')
+    cleaned_host = splited_host[0] + '.' + splited_host[1] + '.'\
+        + splited_host[2] + '.'
 
-if op_system == "Windows":
-    ping1 = "ping -n 1 "
-elif op_system == "Linux":
-    ping1 = "ping -c 1 "
-else:
-    ping1 = "ping -c 1 "
-t1 = datetime.now()
-print("Scanning in Progress:")
+    start_ping = int(input("Диапазон сканирования ОТ: "))
+    ended_ping = int(input("Диапазон сканирования ДО: ")) + 1
+    op_system = platform.system()
 
-for ip in range(st1, en1):
-    addr = net2 + str(ip)
-    comm = ping1 + addr
-    response = os.popen(comm)
+    if op_system == "Windows":
+        ping_command = "ping -n 2 "
+    elif op_system == "Linux":
+        ping_command = "ping -c 2 "
+    else:
+        ping_command = "ping -c 2 "
+    time_before = datetime.now()
+    print("Сканирование запущено:")
 
-    for line in response.readlines():
-        if line.count("TTL"):
-            break
-        if line.count("TTL"):
-            print(addr, "--> Live")
+    for ip in range(start_ping, ended_ping):
+        addr = cleaned_host + str(ip)
+        command = ping_command + addr
+        response = os.popen(command)
+        temp_flag = False
 
-t2 = datetime.now()
-total = t2 - t1
-print("Scanning completed in: ", total)
+        for line in response.readlines():
+            if "ttl" in line:
+                temp_flag = True
+
+        if not temp_flag:
+            print(addr, "--> OFF")
+        else:
+            print(addr, "--> ON")
+
+    time_after = datetime.now()
+    total = time_after - time_before
+    print("Сканирование окончено за: ", total)
+    sleep(300)
