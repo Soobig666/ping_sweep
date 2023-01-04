8a07dfba24097acecee93fea0213cd0db97e7495
soobig666 <darkvader.ukr.net@gmail.com>
2023-01-04 15:05:53 +0200

    refactoring

--- app/main.py
+++ app/main.py
@@ -3,33 +3,34 @@ import platform
 
 from time import sleep
 from datetime import datetime
-from work_with_datas import data_read_from_file
+from work_with_datas import data_read_from_file, data_write_file
 
 
 # KeyboardInterrupt
 while True:
     file = data_read_from_file()
-    ip_list = [number["name"] for number in file]
+
     op_system = platform.system()
     ping_command = "ping -n 2 " if op_system == "Windows" else "ping -c 2 "
     time_before = datetime.now()
-    print("Сканирование запущено:")
+    print("Сканирование запущено В: " + str(time_before))
 
-    for ip in ip_list:
-        command = ping_command + ip
+    for ip in file:
+        command = ping_command + ip["ip_address"]
         response = os.popen(command)
-        temp_flag = False
+        flag = False
 
         for line in response.readlines():
             if "ttl" in line:
                 temp_flag = True
 
-        if not temp_flag:
-            print(ip, "--> OFF")
+        if not flag:
+            ip["status"] = "OFF"
         else:
-            print(ip, "--> ON")
+            ip["status"] = "ON"
 
     time_after = datetime.now()
     total = time_after - time_before
     print("Сканирование окончено за: ", total)
+    data_write_file(file)
     sleep(300)
--- app/work_with_datas.py
+++ app/work_with_datas.py
@@ -1,10 +1,16 @@
 import json
-
-
 # from_file = json.load(opened_file)
 # json.dump(from_file, opened_file)
 
-def data_read_from_file() -> dict:
+
+def data_read_from_file() -> list:
     with open("app/users_dict.json", "r") as opened_file:
         from_file = json.load(opened_file)
+        opened_file.close()
     return from_file
+
+
+def data_write_file(info: list) -> None:
+    with open("app/users_dict.json", "w") as opened_file:
+        json.dump(info, opened_file)
+        opened_file.close()
