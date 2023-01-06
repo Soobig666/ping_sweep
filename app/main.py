f9b1dcc0fc7cc606797e438d0431d6c2e2defb77
soobig666 <darkvader.ukr.net@gmail.com>
2023-01-06 10:58:18 +0200

    refactoring

--- app/main.py
+++ app/main.py
@@ -1,38 +1,47 @@
 import os
 import platform
+import threading
+from sys import argv
 
 from time import sleep
 from datetime import datetime
-from work_with_datas import data_read_from_file, data_write_file
+from datas import data_read_from_file, data_write_file
 from check_packet_loss import read_from_console
 
 
 def ping() -> None:
-    while True:
-        file = data_read_from_file()
-        op_system = platform.system()
-        ping_command = "ping -n 2 " if op_system == "Windows" else "ping -c 2 "
-        time_before = datetime.now()
-        print("Сканирование запущено в " + str(time_before))
+    try:
+        while True:
+            file = data_read_from_file()
+            op_system = platform.system()
+            ping_command = "ping -n 2 " if op_system == "Windows" else "ping -c 2 "
+            time_before = datetime.now()
+            print("\n")
+            print("SCAN IP START ON " + str(time_before))
 
-        for ip in file:
-            command = ping_command + ip["ip_address"]
-            response = os.popen(command)
-            percent_loss = read_from_console(response.readlines())
-            if percent_loss == 100:
-                ip["status"] = "OFF"
-            elif percent_loss <= 99:
-                ip["status"] = "NEED CHECK"
-                print(ip)
-            else:
-                ip["status"] = "ON"
+            for ip in file:
+                command = ping_command + ip["ip_address"]
+                response = os.popen(command)
+                percent_loss = read_from_console(response.readlines())
+                if percent_loss == 100:
+                    ip["status"] = "OFF"
+                elif percent_loss <= 99:
+                    ip["status"] = "NEED CHECK"
+                    print(ip)
+                else:
+                    ip["status"] = "ON"
 
-        time_after = datetime.now()
-        total = time_after - time_before
-        print("Сканирование окончено за: ", total)
-        data_write_file(file)
-        sleep(300)
+            time_after = datetime.now()
+            total = time_after - time_before
+            print("SCAN IP COMPLETED IN: ", total)
+            data_write_file(file)
+            sleep(300)
+    except KeyboardInterrupt:
+        print("\n")
+        print("SCANNING IS STOP!")
 
 
 if __name__ == '__main__':
+    # temp = input(argv)
+    # print(temp)
     ping()
--- app/print.py
+++ app/print.py
@@ -1,6 +1,25 @@
-from work_with_datas import data_read_from_file
+from datas import data_read_from_file
+from typing import Any
 
 
-for line in data_read_from_file():
-    print(line, end="\n")
+def check_command(read: str) -> Any:
+    pass
 
+
+def set_info(command: str) -> Any:
+    pass
+
+
+def upd_info(command: str) -> Any:
+    pass
+
+
+def del_info(command: str) -> Any:
+    pass
+
+
+if __name__ == '__main__':
+    for line in data_read_from_file():
+        print(line, end="\n")
+
+    info_from_terminal = check_command(input("new/upd/del"))
