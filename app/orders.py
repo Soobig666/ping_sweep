40957237e0844ba89132c298c7f636592919c8ef
soobig666 <darkvader.ukr.net@gmail.com>
2023-01-06 17:00:46 +0200

    refactoring2

--- app/orders.py
+++ app/orders.py
@@ -36,13 +36,42 @@ def add_info() -> Any:
     print("ADD info successful!")
 
 
-def upd_info(command: str) -> Any:
+def upd_info(ip_address: str) -> Any:
+    load_info = data_read_from_file()
+    for user in load_info:
+        if user["ip_address"] == ip_address:
+            dict_index = load_info.index(user)
 
-    pass
+            while True:
+                chose = input("\nip_address/name/stop ")
+                if chose == "ip_address":
+                    user[chose] = input("\nEnter new ip_address: ")
+                    print("\nDelete was successful!")
+                    break
+                elif chose == "name":
+                    user[chose] = input("\nEnter new name: ")
+                    print("\nDelete was successful!")
+                    break
+                elif chose == "stop":
+                    print("\nMode 'upd' was stopped")
+                    break
+                else:
+                    print("Incorrect attribute-name")
+            data_write_file(load_info)
+            break
+        print(f"\nAfter check {ip_address} is not found!")
 
 
-def del_info(command: str) -> Any:
-    pass
+def del_info(ip_address: str) -> Any:
+    load_info = data_read_from_file()
+    for user in load_info:
+        if user["ip_address"] == ip_address:
+            dict_index = load_info.index(user)
+            load_info.pop(dict_index)
+            data_write_file(load_info)
+            print("\nDelete was successful!")
+            break
+    print(f"\nAfter check {ip_address} is not found!")
 
 
 def print_info() -> Any:
